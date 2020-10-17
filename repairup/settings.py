be55950c2ad495d87a90b87bf8dba19969fcb323
Wine Kittipop <37920511+kittipop1998@users.noreply.github.com>
2020-10-17 23:51:32 +0700

    commitY

--- repairup/settings.py
+++ repairup/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+
+MEDIA_URL = '/media/'
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
